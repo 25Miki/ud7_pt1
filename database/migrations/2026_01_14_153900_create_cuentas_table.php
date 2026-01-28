8150f61252307520e6f2b150ae250470b541d2fb
25miki <mariamarchantellopis@gmail.com>
2026-01-28 16:46:21 +0100

    He añadido el resto de cosas de la práctica pero no funciona

---
+++ database/migrations/2026_01_14_153900_create_cuentas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cuentas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('codigo')->unique();
+
+            $table->foreignId('cliente_id')->nullable()
+                    ->constrained('clientes')
+                    ->onDelete('cascade');
+
+            $table->decimal('saldo', 10, 2);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cuentas');
+    }
+};
